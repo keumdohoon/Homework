a24c4947d95abbcedfc4b5e7159e2ac81744133b
keumdohoon <58944180+keumdohoon@users.noreply.github.com>
2020-05-24 14:22:56 +0900

    202005241422

---
+++ p.69_statistics.py
@@ -0,0 +1,39 @@
+#p.69_statistics
+
+num_friends = [100.0,49,41,40,25,21,21,19,19,18,18,16,15,15,15,15,14,14,13,13,13,13,12,12,11,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,8,8,8,8,8,8,8,8,8,8,8,8,8,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+
+from collections import Counter
+import matplotlib.pyplot as plt
+
+friend_counts = Counter(num_friends)
+xs = range(101)                         # 최대값 100
+ys = [friend_counts[x] for x in xs]     # 높이는 해당 친구의 수를 갖고 있는 사용자 수.
+plt.bar(xs, ys)
+plt.axis([0, 101, 0, 25])
+plt.title("Histogram of Friend Counts")
+plt.xlabel("# of friends")
+plt.ylabel("# of people")
+# plt.show()
+
+num_points = len(num_friends)               # 204
+
+
+assert num_points == 204
+
+largest_value = max(num_friends)            # 100
+smallest_value = min(num_friends)           # 1
+
+
+assert largest_value == 100
+assert smallest_value == 1
+
+sorted_values = sorted(num_friends)
+smallest_value = sorted_values[0]           # 1
+second_smallest_value = sorted_values[1]    # 1
+second_largest_value = sorted_values[-2]    # 49
+
+
+assert smallest_value == 1
+assert second_smallest_value == 1
+assert second_largest_value == 49
+
